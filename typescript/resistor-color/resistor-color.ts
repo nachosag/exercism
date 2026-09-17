6ffb9129b5e507dc97dd1819831d691c00fdaa1c
nachosag <ignacioborlenghi@gmail.com>
2026-09-17 19:08:07 -0300

    add: solution for resistor-color

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,29 @@
+export const colorCode = (color: string): number => {
+  const colors: Record<string, number> = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+  }
+
+  return colors[color]
+}
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+]
